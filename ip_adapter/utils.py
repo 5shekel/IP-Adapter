bc568f34c4a107afdd29a5fffc49cc27d861d216
yair <yair99@gmail.com>
2023-09-17 16:45:31 +0300

    from diffusers.utils.torch_utils import is_compiled_module

--- ip_adapter/utils.py
+++ ip_adapter/utils.py
@@ -6,7 +6,8 @@ import numpy as np
 import PIL.Image
 import torch
 import torch.nn.functional as F
-from diffusers.utils import is_compiled_module
+# from diffusers.utils import is_compiled_module
+from diffusers.utils.torch_utils import is_compiled_module
 from diffusers.pipelines.controlnet.multicontrolnet import MultiControlNetModel
 from diffusers.models import ControlNetModel
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
